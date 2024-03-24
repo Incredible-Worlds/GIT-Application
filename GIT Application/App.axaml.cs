a86e384f78dc298cad6657ca4cc89649865bc2a7
KamysDev <work.kamysdev@gmail.com>
2024-03-24 18:13:45 +0700

    Добавьте файлы проекта.

---
+++ GIT Application/App.axaml.cs
@@ -0,0 +1,29 @@
+using Avalonia;
+using Avalonia.Controls.ApplicationLifetimes;
+using Avalonia.Markup.Xaml;
+using GIT_Application.ViewModels;
+using GIT_Application.Views;
+
+namespace GIT_Application
+{
+    public partial class App : Application
+    {
+        public override void Initialize()
+        {
+            AvaloniaXamlLoader.Load(this);
+        }
+
+        public override void OnFrameworkInitializationCompleted()
+        {
+            if (ApplicationLifetime is IClassicDesktopStyleApplicationLifetime desktop)
+            {
+                desktop.MainWindow = new MainWindow
+                {
+                    DataContext = new MainWindowViewModel(),
+                };
+            }
+
+            base.OnFrameworkInitializationCompleted();
+        }
+    }
+}
---
+++ GIT Application/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,7 @@
+﻿namespace GIT_Application.ViewModels
+{
+    public class MainWindowViewModel : ViewModelBase
+    {
+
+    }
+}
