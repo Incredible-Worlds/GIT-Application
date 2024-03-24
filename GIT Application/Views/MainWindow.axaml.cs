9fe2cffc5b5740a5244c0ac2004de3d4e2081e57
KamysDev <work.kamysdev@gmail.com>
2024-03-24 18:57:36 +0700

    Created header for main window

--- GIT Application/Views/MainWindow.axaml.cs
+++ GIT Application/Views/MainWindow.axaml.cs
@@ -1,4 +1,6 @@
 using Avalonia.Controls;
+using Avalonia.Input;
+using System.Windows.Input;
 
 namespace GIT_Application.Views
 {
@@ -6,7 +8,15 @@ namespace GIT_Application.Views
     {
         public MainWindow()
         {
+            this.SystemDecorations = SystemDecorations.None;
+            this.ExtendClientAreaToDecorationsHint = true;
+            this.ExtendClientAreaTitleBarHeightHint = 30;
             InitializeComponent();
+        }
+
+        private void Menu_PointerPressed(object? sender, Avalonia.Input.PointerPressedEventArgs e)
+        {
+            BeginMoveDrag(e);
         }
     }
 }
