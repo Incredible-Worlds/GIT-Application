6aa5eac47c1bf3845b4909998eab473ef9d20655
Tea_Cup <124342276+Bell404ll@users.noreply.github.com>
2024-03-25 01:39:20 +0700

    Design is ALMOST done, polishing required

--- GIT Application/Views/MainWindow.axaml.cs
+++ GIT Application/Views/MainWindow.axaml.cs
@@ -6,7 +6,15 @@ namespace GIT_Application.Views
     {
         public MainWindow()
         {
+            this.SystemDecorations = SystemDecorations.None;
+            this.ExtendClientAreaToDecorationsHint = true;
+            this.ExtendClientAreaTitleBarHeightHint = 30;
             InitializeComponent();
+        }
+
+        private void Menu_PointerPressed(object? sender, Avalonia.Input.PointerPressedEventArgs e)
+        {
+            BeginMoveDrag(e);
         }
     }
 }
