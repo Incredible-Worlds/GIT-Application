bc0bd066a9e8112dbe79ce416e26eeb88ad64b61
Tea_Cup <124342276+Bell404ll@users.noreply.github.com>
2024-03-24 19:18:25 +0700

    Start developing Repository Window

---
+++ GIT Application/Views/RepositoryPage.axaml.cs
@@ -0,0 +1,12 @@
+using Avalonia.Controls;
+
+namespace GIT_Application.Views
+{
+    public partial class RepositoryPage : Window
+    {
+        public RepositoryPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
